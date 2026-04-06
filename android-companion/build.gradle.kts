d6e90fffb8f4da06d3d0b8bf3ae257badd742bfa
ping840907 <tom840907@gmail.com>
2026-04-06 20:27:29 +0800

    Add files via upload

---
+++ android-companion/build.gradle.kts
@@ -0,0 +1,5 @@
+// Peleprompter Android Companion - Top-level build file
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
